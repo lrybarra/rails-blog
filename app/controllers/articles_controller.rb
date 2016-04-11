82f02d8d579a18afdac708238960db1f23cda5f0
Luis Ybarra <lrybarra@gmail.com>
2016-04-11 15:27:31 -0500

    yooooo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "lulu", password: "chicago", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
